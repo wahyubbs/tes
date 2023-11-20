3fc8ea88d9dfaf9e97f43bd09198a1a56dea24ae
wahyubbs <wahyusrp.bbs@gmail.com>
2023-11-20 16:34:55 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    minimumCacheTTL: 60,
     domains: ["storage.googleapis.com"],
     formats: ["image/avif", "image/webp"],
   },
